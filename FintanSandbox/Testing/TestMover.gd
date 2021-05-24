2f61f1fd1b3e41696c95b37dd7b1e476b94e7856
craigslisttrashman <fintanneff@gmail.com>
2021-05-24 19:29:18 -0400

    Set up a mockup of the main scene
    Set up a mockup of the main scene

--- FintanSandbox/Testing/TestMover.gd
+++ FintanSandbox/Testing/TestMover.gd
@@ -3,10 +3,10 @@ extends KinematicBody2D
 func _ready():
 	pass
 
-func _process(delta):
+func _physics_process(delta):
 	var inp = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		0
 	)
-	move_and_collide(inp * delta * 30);
+	move_and_collide(inp);
 	pass
