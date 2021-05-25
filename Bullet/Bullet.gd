5949bff3f6aaca9c71b89d51646497d8f935acee
craigslisttrashman <fintanneff@gmail.com>
2021-05-24 23:37:36 -0400

    Shooting is starting to work
    You can now shoot in 5ds

---
+++ Bullet/Bullet.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var vel = Vector2.ZERO
+var rot = 0.0
+var dedtimer = 60
+
+func _physics_process(delta):
+	if (dedtimer <= 0):
+		queue_free()
+	else:
+		dedtimer -= 1
+	transform.origin.x += vel.rotated(rot).x
+	transform.origin.y += vel.rotated(rot).y
+
+func angleshot(v, r):
+	vel = v
+	rot = r
--- Player/Player.gd
+++ Player/Player.gd
@@ -9,6 +9,8 @@ var animationState = 0
 onready var animator = $AnimationPlayer
 onready var sprite = $Sprite
 
+onready var bullet = preload("res://Bullet/Bullet.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animator.play("Idle", -1, 1, false) 
@@ -28,17 +30,26 @@ func _physics_process(delta):
 	if(abs(inputVector.x) > 0.5 && !Input.is_action_pressed("ui_hold")):
 		if(inputVector.y < -.4):
 			animator.play("WalkLeftAngled", -1, 1, false)
+			angle = Vector2(sign(inputVector.x), -1)
 		else:
 			animator.play("WalkLeft", -1, 1, false)
+			angle = Vector2(sign(inputVector.x), 0)
 		move_and_collide(sign(inputVector.x) * Vector2.RIGHT * moveSpeed)
 	else:
 		if (inputVector.y < -.6 && abs(inputVector.x) < 0.5):
 			animator.play("UpIdle", -1, 1, false)
+			angle = Vector2(0, -1)
 		elif (inputVector.y < -.4):
 			animator.play("AngleIdle", -1, 1, false)
 		else:
 			animator.play("Idle", -1, 1, false)
-
-	
-	
-	pass
+			angle = Vector2(-sprite.scale.x, 0)
+			
+	#Shooting
+	if (Input.is_action_just_pressed("ui_shoot")):
+		print("Shoot!")
+		var newbullet = bullet.instance()
+		newbullet.transform.origin.x = transform.origin.x
+		newbullet.transform.origin.y = transform.origin.y
+		newbullet.angleshot(angle.normalized() * 4, 0)
+		get_parent().add_child(newbullet)
