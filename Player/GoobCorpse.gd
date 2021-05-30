ec5baff841b5b7ac80c037dc0d0ac0c204ee08e9
craigslisttrashman <fintanneff@gmail.com>
2021-05-30 17:03:45 -0400

    Random nasty sound
    Random nasty sound

--- Player/GoobCorpse.gd
+++ Player/GoobCorpse.gd
@@ -1,9 +1,15 @@
 extends Sprite
 
 var vely = -2
+onready var bs = $based_sound
+onready var cs = $cringe_sound
 
 func _ready():
-	
+	var roll = rand_range(0, 200)
+	if (roll < 2):
+		bs.play()
+	else:
+		cs.play()
 	scale = Vector2.ONE * 2
 
 func _physics_process(delta):
