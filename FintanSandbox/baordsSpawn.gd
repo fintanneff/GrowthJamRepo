00682bb713538b8446e7c0bdd3a2a2d6dee2d1fe
Kris Camp <rikscamp@gmail.com>
2021-05-30 23:17:44 -0400

    boards

---
+++ FintanSandbox/baordsSpawn.gd
@@ -0,0 +1,32 @@
+extends Sprite
+
+var done = false
+var starty
+export(int) var score_limit
+
+func _ready():
+	ScoreTracker.updatable.push_front(self)
+	starty = transform.origin.y
+	transform.origin.y += 300
+	modulate.a = 0
+	
+	
+	var roll = int(rand_range(0,4))
+	print (roll)
+	
+	frame = roll
+	pass
+
+func on_score_update(score):
+	if (score >= score_limit && !done):
+		done = true
+
+func _physics_process(delta):
+	if (done):
+		if (modulate.a < 0.995):
+			modulate.a = lerp(modulate.a, 1, 0.005)
+			transform.origin.y = lerp(transform.origin.y, starty, 0.01)
+		else:
+			var me = ScoreTracker.updatable.find(self)
+			ScoreTracker.updatable.remove(me)
+			set_script(null)
